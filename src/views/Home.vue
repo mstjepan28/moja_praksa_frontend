548806bfaccad82038482963c50be85ee9fec53b
mstjepan28 <markovcics@gmail.com>
2020-07-06 20:21:00 +0200

    Slideshow

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,49 +1,52 @@
 <template>
-  <div>
-      <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
-        <div class="carousel-inner">
-          <div class="carousel-item active">
-            <img class="d-block w-100" src="" alt="First slide">
-          </div>
-          <div class="carousel-item">
-            <img class="d-block w-100" src="" alt="Second slide">
-          </div>
-          <div class="carousel-item">
-            <img class="d-block w-100" src="" alt="Third slide">
-          </div>
-        </div>
-      </div>  
+  <div style="width: 100%">
+    <vue-flux
+      :options="vfOptions"
+      :images="vfImages"
+      :transitions="vfTransitions"
+      ref="slider">
+
+      <template v-slot:preloader>
+          <flux-preloader />
+      </template>
+    </vue-flux>
+
+
   </div>
 </template>
 
 <script>
-//import ScrollCard from '@/components/scroll_card.vue'
+  import {
+    VueFlux,
+    FluxPreloader
+  } from 'vue-flux';
 
-export default {
-  data(){
-    return{
-      scroll_cards: [
-        {img_src: 'https://images.unsplash.com/photo-1531297484001-80022131f5a1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=707&q=80'},
-        {img_src: 'https://images.unsplash.com/photo-1531297484001-80022131f5a1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=707&q=80'},
-        {img_src: 'https://images.unsplash.com/photo-1531297484001-80022131f5a1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=707&q=80'},
-        {img_src: 'https://images.unsplash.com/photo-1531297484001-80022131f5a1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=707&q=80'},
-        {img_src: 'https://images.unsplash.com/photo-1531297484001-80022131f5a1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=707&q=80'},
-        {img_src: 'https://images.unsplash.com/photo-1531297484001-80022131f5a1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=707&q=80'},
-      ]
-    }
-  },
-  name: 'Home',
-  //components: { ScrollCard }
-}
+  export default {
+    components: {
+        VueFlux,
+        FluxPreloader
+    },
+
+    data: () => ({
+        vfOptions: {
+          autoplay: true,
+          aspectRatio: '16:5'
+        },
+        vfImages: [ 
+          'https://images.unsplash.com/photo-1531297484001-80022131f5a1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=707&q=80', 
+          'https://images.unsplash.com/photo-1593642532871-8b12e02d091c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80', 
+          'https://images.unsplash.com/photo-1504639725590-34d0984388bd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=667&q=80' 
+        ],
+        vfTransitions: [ 'fade' ]
+    }),
+    name: 'Home'
+  }
 </script>
 
 <style scoped>
-.scrolling-wrapper{
-  overflow-x: scroll;
-  overflow-y: hidden;
-  white-space: nowrap;
-}
-.scrolling-wrapper {
-  -webkit-overflow-scrolling: touch;
+.image_display{
+  width: 100%;
+  height: 150px;
+
 }
 </style>
